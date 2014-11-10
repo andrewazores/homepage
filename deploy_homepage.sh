7b4c75825c0336342cf09a2ac5e607537b0704db
Andrew Azores <andrew.azores@gmail.com>
2014-11-10 01:43:15 -0500

    added prompts and deploy dir as arg

--- deploy_homepage.sh
+++ deploy_homepage.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
+function abort {
+    echo "Aborting..."
+    exit 0
+}
+
+if [ $# -ne 1 ]; then
+    echo "usage: deploy_homepage.sh www_deploy_directory"
+    exit 1
+else
+    echo "Are you sure you want to deploy the homepage to $1? Y/n [n]"
+    read ans
+    if [ "$ans" = "Y" -o "$ans" = "y" ]; then
+        if [ ! -d "$1" ]; then
+            echo "The directory $1 does not exist. Create it (and all parents)? Y/n [n]"
+            read ans
+            if [ "$ans" = "Y" -o "$ans" = "y" ]; then
+                echo "Creating $1..."
+                mkdir -p "$1"
+            else
+                abort
+            fi
+        fi
+        echo "Building..."
+    else
+        abort
+    fi
+fi
+
 command -v asciidoctor > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     ADOC="asciidoctor"
@@ -14,4 +42,4 @@ else
 fi
 
 $ADOC index.adoc
-sudo cp index.html /srv/http/
+sudo cp -i index.html $1
